639489dc73ab9a89e7d04525e06bffe202c2854f
Michael <mobrien@colgate.edu>
2021-09-27 16:03:00 -0400

    added notes for future on recipecard.js

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import { Card } from "semantic-ui-react"
 
+// functional component using the Card component from semantic
+// will require <Image> in the future from semantic
+// potentially changed into a class component downthe road, but unlikely
+// only presents information, but can connect to context for stretch goals such as "liking"
+
+
 function RecipeCard() {
     return(
     <Card>
