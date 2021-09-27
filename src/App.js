c21abc577cda23a24206fe1948857df9804eedff
Michael <mobrien@colgate.edu>
2021-09-27 16:04:11 -0400

    added semantic-ui-css, imported RecipeCard and made sure it worked

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import RecipeCard from './components/RecipeCard'
 
 function App() {
   return (
@@ -7,6 +9,9 @@ function App() {
       <header>
           <p>Recipe-holder app (placeholder name)</p>
       </header>
+      <div id="body-container">
+        <RecipeCard />
+      </div>
     </div>
   );
 }
