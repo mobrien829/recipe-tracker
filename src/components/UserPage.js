d9bd011ae71a13034fa2331db213fdf469bfae44
Michael <mobrien@colgate.edu>
2021-09-29 15:36:44 -0400

    created user page null for testing routes

---
+++ src/components/UserPage.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { Container } from "semantic-ui-react"
+
+const UserPage = () => {
+    console.log("user page rendering")
+    return null
+}
+
+export default UserPage
