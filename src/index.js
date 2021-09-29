191289f123446fb878e59ef0ac097b7254084e91
Michael <mobrien@colgate.edu>
2021-09-29 15:04:51 -0400

    added react-router-dom and browserrouter to index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { UserContextProvider } from "./contexts/user-context"
+import { BrowserRouter as Router } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
     <UserContextProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </UserContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
