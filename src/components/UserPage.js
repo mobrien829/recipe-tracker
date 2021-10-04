3a332c9d7e021399f4b55e346795fbc813152afa
Michael <mobrien@colgate.edu>
2021-10-04 17:18:04 -0400

    set up skeleton of user page, set TODOS for future

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,9 +1,18 @@
 import React from "react"
 import { Container } from "semantic-ui-react"
 
+// TODO: recipe list mapping or scrollable?
+// create backend seeding and start pulling info from backend rather than hardcode
+
 const UserPage = () => {
     console.log("user page rendering")
-    return null
+    return (
+        <>
+        <Container className="userName">Peyton Manning</Container>
+        <Container className="about">About</Container>
+        <Container className="recipeList">My Recipes</Container>
+        </>
+    )
 }
 
 export default UserPage
