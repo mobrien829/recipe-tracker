3534612a5d7bf2bc8caf152c21e0704e36f9f07c
Michael <mobrien@colgate.edu>
2021-10-07 15:12:02 -0400

    created login component, intend to either make it a singular component or its functionality will be built in to navbar

---
+++ src/components/Login.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { Container, Button } from "semantic-ui-react"
+
+const Login = () => { 
+    console.log("rendering login")
+    return null
+}
+
+export default Login
