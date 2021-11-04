c7242c85c6c7fcbea4263aae277c93ade825a8e1
Michael <mobrien@colgate.edu>
2021-11-04 18:18:06 -0400

    set up imports and wrote todos

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,18 @@
-import React from "react"
+import React, { useState, useContext } from "react"
+import { UserContext } from "../contexts/user-context"
+import { useHistory } from "react-router-dom"
 import { Container, Button } from "semantic-ui-react"
 
+
+// TODO: set up state and login. afterwards need to set up new users.
 const Login = () => { 
+
+    const [user, setUser] = useContext(UserContext)
+
     console.log("rendering login")
-    return null
+    return (
+        <Container></Container>
+    )
 }
 
 export default Login
