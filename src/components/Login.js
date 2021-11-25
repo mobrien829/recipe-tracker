97e9b43d6f17fdacd8e3096c702d1589bee27566
Michael <mobrien@colgate.edu>
2021-11-24 23:32:34 -0500

    finished components for login form, need to finish setting up the auth there

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,47 @@
 import React, { useState, useContext } from "react"
 import { UserContext } from "../contexts/user-context"
 import { useHistory } from "react-router-dom"
-import { Container, Button } from "semantic-ui-react"
+import { Container, Button, Form, Input, TextArea } from "semantic-ui-react"
 
 
 // TODO: set up state and login. afterwards need to set up new users.
+// finish login helper to validate logins
 const Login = () => { 
+
+    let history = useHistory()
 
     const [user, setUser] = useContext(UserContext)
 
+    const handleLogin = () => {
+        console.log("logging in")
+        // history.push("/")
+    }
+
     console.log("rendering login")
     return (
-        <Container></Container>
+        <Container>
+            <Form>
+                <Form.Field 
+                id="login-email"
+                control={Input}
+                label="Email"
+                placeholder="email"
+                />
+                <Form.Field 
+                id="login-password"
+                label="Password" 
+                control={Input}
+                type="password"
+                placeholder="password"
+                />
+                <Form.Field 
+                id="login-submit"
+                control={Button}
+                content="Login"
+                onClick={handleLogin}
+                />
+            </Form>
+        </Container>
     )
 }
 
